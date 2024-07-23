e36ebf260037fda52e237abe848c6296a1cf5618
Abhishek kumar <89406337+AbhishekKr2001@users.noreply.github.com>
2024-07-23 10:56:30 +0530

    Add files via upload

---
+++ Frontend/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css';
+export default function Home() {
+    return (<>
+        <div className="div">
+            <div className='div1'>
+
+                <h1 className='h1e'>MUSIC LIBRARY</h1>
+                <h2 className='h1e'>All Songs in a Single App</h2></div></div>
+
+    </>
+
+    )
+}
